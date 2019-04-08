e7d1242fd0420f297693eddd4458a5bbb14ef296
Herbert Lange <herbert.lange@cse.gu.se>
2019-04-08 19:28:04 +0200

    major rewrite to make code more readable and data more intuitive (i hope)

--- Tool/Tree.hs
+++ Tool/Tree.hs
@@ -5,115 +5,155 @@ import PGF.Internal
 import qualified Data.Map.Strict as M
 import Data.Maybe
 import Data.List
+import SAT
+import SAT.Bool
+import System.IO.Unsafe (unsafePerformIO)
+import Control.Monad
 
+-- Boolean operations 
 data SAT a = SVar a | Conj [SAT a] | Dis [SAT a] | Imp (SAT a) (SAT a)
 
+-- Ways to generate LP formats
 data LPFormat = LPSolve | CPLEX ;
 data ConstraintFormat = AMPL | LP LPFormat ; -- | MPS String ;
 
-data Constraint a = C { rules :: [a], trees :: [[(a,[a])]] } deriving Show; 
+-- | A list of rule names
+type Rules a = [a]
+-- | A list of trees, i.e. a list of list of rule names
+type TreeRules a = [Rules a]
+-- | A list of sentences, i.e .a list of a list of rule names
+type SentenceTrees a = [TreeRules a]
 
+-- Simple way to model our problem:
+-- It has the following components:
+-- - A grammar has a set of rules
+-- - For a set of sentences we can get the for each sentence a list of trees
+-- - Each tree is a list of syntax rules
+-- The list of syntax rules can be a set or a multiset, i.e. contain elements repeatedly
+data ConstraintProblem a = CP { rules :: Rules a, trees :: SentenceTrees a } deriving Show;
+
+-- Same a above but labeled on sentence and tree level
+data LabeledConstraintProblem a = LCP { ltrees :: [(String,[(String,[a])])] } deriving Show ;
+
+-- Make SAT show-able
 instance Show a => Show (SAT a) where
   show (SVar a) = show a
-  show (Conj ts) = intercalate " /\\ " (map show ts)
-  show (Dis ts) = intercalate " \\/ " (map show ts)
+  show (Conj ts) = "(" ++ intercalate " /\\ " (map show ts) ++ ")"
+  show (Dis ts) =  "(" ++ intercalate " \\/ " (map show ts) ++ ")"
   show (Imp p c) = show p ++ " => " ++ show c ++ "\n"
-  
+
+-- | Function to create a constraint problem based on a PGF grammar and a list of (positive)
+-- | example sentences
+mkMultisetProblem :: PGF -> [String] -> ConstraintProblem String
+mkMultisetProblem pgf sents =
+  let
+    rules = [showCId r | r <- functions pgf]
+    treeSets = map (sentenceTrees pgf) sents
+    funSets = (map . map) (map showCId . treeFunctions) treeSets
+  in
+    CP rules funSets
+
+-- | Function to convert the lists of syntax rules from multi-sets to sets
+convertToSetProblem :: Eq a => ConstraintProblem a -> ConstraintProblem a
+convertToSetProblem (CP rules trees) = CP rules $ (map . map) nub trees
+
+-- | Function to add labels to a constraint problem. The labels have the shape
+-- | s0..sn and t0..tm and can be used as variables in the constraint solving
+convertToLabeledProblem :: ConstraintProblem a -> LabeledConstraintProblem a
+convertToLabeledProblem (CP _ trees) =
+  (LCP [("s" ++ show si,[("s" ++ show si ++ "t" ++ show ti,rs) | (ti,rs) <- zip [0..] ts]) | (si,ts) <- zip [0..] trees])
+  -- (LCP (zip ["s" ++ show i | i <- [0..]] (label "t" trees)))
+  -- where
+  --   label :: String -> [[a]] -> [[(String,a)]]
+  --   label l ll =
+  --     splitAts (map length ll) (zip [l ++ show i | i <- [0..]] (concat ll))
+
+-- | Function to parse a sentence to a list of trees using a PGF grammar
 sentenceTrees :: PGF -> String -> [Tree]
 sentenceTrees pgf sent =
   parse pgf (head $ languages pgf) (startCat pgf) sent
-  
-sentenceFunctions :: PGF -> String -> [[CId]]
-sentenceFunctions pgf sent =
-  let
-    parses = parse pgf (head $ languages pgf) (startCat pgf) sent
-  in
-    map treeToFunList parses
 
-sentenceFunctionsCount :: PGF -> String -> [M.Map String Int]
-sentenceFunctionsCount pgf sent =
-  let
-    lstToMap = Prelude.foldl (\m k -> M.alter (maybe (Just 1) (\n -> Just (n + 1))) (showCId k) m) M.empty
-    funLists = sentenceFunctions pgf sent
-  in
-    map lstToMap funLists
+-- | Function to convert a tree to a list of syntactic functions
+treeFunctions :: Tree -> [CId]
+treeFunctions (EApp e1 e2) = treeFunctions e1 ++ treeFunctions e2
+treeFunctions (EFun f) = [f]
+treeFunctions _ = []
 
-sentenceSat :: PGF -> String -> SAT String
-sentenceSat pgf sent =
+-- | Function to count syntax rules in a constraint problem
+funCounts :: ConstraintProblem String -> M.Map String Int
+funCounts (CP rules trees) =
   let
-    funLists = sentenceFunctions pgf sent
+    lstToMap :: [String] -> M.Map String Int
+    lstToMap = Prelude.foldl (\m k -> M.alter (maybe (Just 1) (\n -> Just (n + 1))) k m) M.empty
   in
-    Dis (map mkSatImp $ zip ["t" ++ show i | i <- [0..]] funLists)
+    M.unionsWith (+) $ (concatMap . map) lstToMap trees
 
-multiSentSat :: PGF -> [String] -> SAT String
-multiSentSat pgf sents =
+-- | Function to convert a constraint problem to a SAT formula
+convertToMiniSat :: (Eq a,Show a) => ConstraintProblem a -> (SAT String, SAT String)
+convertToMiniSat cp =
   let
-    funListsS = map (sentenceFunctions pgf) sents
-    listLengths = map length funListsS
+    (LCP labeled) = convertToLabeledProblem $ convertToSetProblem cp
   in
-    Conj $ map (Dis . map mkSatImp) (splitAts listLengths $ zip ["t" ++ show i | i <- [0..]] (concat funListsS) )
+    (Conj $ map (SVar. fst) labeled, Conj (
+        [(Imp (SVar s) (Dis (map (SVar . fst) ts)))|(s,ts) <- labeled] ++
+        [ Imp (SVar t) (Conj (map (SVar . show) rs))| (s,ts) <- labeled, (t,rs) <- ts]))
 
-multiSentConstraints :: PGF -> [String] -> Constraint String
-multiSentConstraints pgf sents =
+-- | Function to print a constraint problem as AMPL
+printConstraints :: ConstraintFormat -> ConstraintProblem String -> String
+printConstraints AMPL cp =
   let
-    rules = map showCId $ functions pgf
-    sats = multiSentSat pgf sents
-  in
-    C rules $ map (map unImp) $ map unDis $ unConj sats
-  where
-    unConj (Conj l) = l
-    unConj _ = []
-    unDis (Dis l) = l
-    unDis _ = []
-    unImp (Imp (SVar s) c) = (s,map unVar $ unConj c)
-    unImp _ = ([],[])
-    unVar (SVar v) = v
-    unVar _ = undefined
-
-printConstraints :: ConstraintFormat -> Constraint String -> String
-printConstraints AMPL (C rules trees) =
-  let
-    treeVars = concatMap (map fst) trees
-    ruleVars = nub $ concatMap (concatMap snd) trees
+    (LCP lts) = convertToLabeledProblem cp
+    vars = [(t,rs) | (s,ts) <- lts, (t,rs) <- ts]
+    treeVars = map fst vars 
+    ruleVars = nub . concatMap snd $ vars
   in
     unlines $
       ["var " ++ v ++ ", binary;" | v <- ruleVars ] ++
       ["var " ++ v ++ ", binary;" | v <- treeVars ] ++
       ["minimize rules : " ++ intercalate " + " ruleVars ++ ";",
        "minimize trees : " ++ intercalate " + " treeVars ++ ";"] ++
-      [ "s.t. cs" ++ show i ++ ": " ++ intercalate " + " c ++ " >= 1 ; " | (i,c) <- zip [0..] (map (map fst) trees)] ++
-      [ "s.t. c" ++ show i ++ ": " ++ c ++ "; " | (i,c) <- zip [0..] [show (length st) ++ " * " ++ ft ++ " - (" ++ intercalate " + " st ++ ") <= 0" | s <- trees, (ft,st) <- s]] ++
+      [ "s.t. cs" ++ show i ++ ": " ++ intercalate " + " c ++ " >= 1 ; " | (i,c) <- zip [0..] [map fst ts | (s,ts) <- lts]] ++
+      [ "s.t. c" ++ show i ++ ": " ++ c ++ "; " | (i,c) <- zip [0..] [show (length rs) ++ " * " ++ t ++ " - (" ++ intercalate " + " rs ++ ") <= 0" | (s,ts) <- lts, (t,rs) <- ts]] ++
       ["solve;",
        "display " ++ intercalate ", " ruleVars ++ ";",
        "display " ++ intercalate ", " treeVars ++ ";"
       ]
-      
-printConstraints (LP LPSolve) (C rules trees) =
+
+-- | Function to print a constraint problem as LPSolve LP
+printConstraints (LP LPSolve) cp =
   let
-    treeVars = concatMap (map fst) trees
+    (LCP lts) = convertToLabeledProblem cp
+    vars = [(t,rs) | (s,ts) <- lts, (t,rs) <- ts]
+    treeVars = map fst vars
+    ruleVars = nub . concatMap snd $ vars 
   in
     clean $ unlines $
-      ["min: " ++ intercalate " + " rules ++ " + " ++ intercalate " + " treeVars ++ ";"] ++
-      [ "cs" ++ show i ++ ": " ++ intercalate " + " c ++ " >= 1 ; " | (i,c) <- zip [0..] (map (map fst) trees)] ++
-      [ "c" ++ show i ++ ": " ++ c ++ "; " | (i,c) <- zip [0..] [show (length st) ++ " * " ++ ft ++ " - " ++ intercalate " - " st ++ " <= 0" | s <- trees, (ft,st) <- s]] ++
-      ["bin " ++ v ++ ";" | v <- rules] ++
+      ["min: " ++ intercalate " + " ruleVars ++ " + " ++ intercalate " + " treeVars ++ ";"] ++
+      [ "cs" ++ show i ++ ": " ++ intercalate " + " c ++ " >= 1 ; " | (i,c) <- zip [0..] [map fst ts | (s,ts) <- lts]] ++
+      [ "c" ++ show i ++ ": " ++ c ++ "; " | (i,c) <- zip [0..] [show (length rs) ++ " * " ++ t ++ " - " ++ intercalate " - " rs ++ " <= 0" | (s,ts) <- lts, (t,rs) <- ts]] ++
+      ["bin " ++ v ++ ";" | v <- ruleVars ] ++
       ["bin " ++ v ++ ";" | v <- treeVars ]
-      
-printConstraints (LP CPLEX) (C rules trees) =
+
+-- | Function to print a constraint problem as CPLEX LP
+printConstraints (LP CPLEX) cp =
   let
-    treeVars = concatMap (map fst) trees
+    (LCP lts) = convertToLabeledProblem cp
+    vars = [(t,rs) | (s,ts) <- lts, (t,rs) <- ts]
+    treeVars = map fst vars
+    ruleVars = nub . concatMap snd $ vars 
   in
-    clean $ unlines $
+    unlines $
       [ "Minimize" ] ++ 
-      [ " obj: " ++ intercalate " + " rules ++ " + " ++ intercalate " + " treeVars ] ++
+      [ " obj: " ++ intercalate " + " ruleVars ++ " + " ++ intercalate " + " treeVars ] ++
       [ "Subject to"] ++ 
-      [ " cs: " ++ show i ++ ": " ++ intercalate " + " c ++ " >= 1 ; " | (i,c) <- zip [0..] (map (map fst) trees)] ++
-      [ " c: " ++ show i ++ ": " ++ c ++ "; " | (i,c) <- zip [0..] [show (length st) ++ " * " ++ ft ++ " - " ++ intercalate " - " st ++ " <= 0" | s <- trees, (ft,st) <- s]] ++
+      [ " cs" ++ show i ++ ": " ++ intercalate " + " c ++ " >= 1" | (i,c) <- zip [0..] [map fst ts | (s,ts) <- lts]] ++
+      [ " c" ++ show i ++ ": " ++ c | (i,c) <- zip [0..] [show (length rs) ++ "" ++ t ++ " - " ++ intercalate " - " rs ++ " <= 0" | (s,ts) <- lts, (t,rs) <- ts]] ++
       [ "Binary"] ++ 
-      [ " " ++ v | v <- rules] ++
+      [ " " ++ v | v <- ruleVars ] ++
       [ " " ++ v | v <- treeVars ] ++
       [ "End" ]
-      
+
+-- Partial implementation of the MPS exporter but the format is a pain in the ass
 -- printConstraints (MPS name) (C rules trees) =
 --   let
 --     treeVars = concatMap (map fst) trees
@@ -133,40 +173,92 @@ printConstraints (LP CPLEX) (C rules trees) =
 --      ["ENDATA"]
 
 
+-- -- Solvers
+
+-- solveCount :: M.Map String Int -> Int -> [String]
+-- solveCount counts cutoff =
+--   M.keys $ M.filter (>= cutoff) counts
+
+-- | Function to solve a SAT provlem using MiniSat. The problem consists
+-- | of two parts, a conjunction(!) of premises and a SAT formula to be
+-- | solved
+solveSat :: (SAT String,SAT String) -> IO [String]
+solveSat sats =
+  do
+    -- Create solver
+    s <- newSolver
+    -- Add solver with the given problem
+    (m,ls) <- satToMiniSat s sats
+    -- Solve with the sentences to be true as a premise
+    b <- solve s ls
+    -- Extract the result from the module
+    if b then return $ M.keys $ M.filter (\l -> unsafePerformIO $ modelValue s l) m
+    else return []
+
 -- Helper functions
 
-treeToFunList :: Tree -> [CId]
-treeToFunList (EApp e1 e2) = treeToFunList e1 ++ treeToFunList e2
-treeToFunList (EFun f) = [f]
-treeToFunList _ = []
+-- -- | Function to split a list at a list of positions
+-- splitAts :: [Int] -> [a] -> [[a]]
+-- splitAts [] _ = []
+-- splitAts _ [] = []
+-- splitAts (i:is) es =
+--   take i es:splitAts is (drop i es)
 
-solveCount :: [[(CId,Int)]] -> [String]
-solveCount = undefined
-
-solveSat :: PGF -> [String] -> [String]
-solveSat pgf sents =
-  undefined -- map (sentenceSat pgf) sentences
-
-splitAts :: [Int] -> [a] -> [[a]]
-splitAts [] _ = []
-splitAts _ [] = []
-splitAts (i:is) es =
-  take i es:splitAts is (drop i es)
-  
-mkSatImp (t,fs) = Imp (SVar t) (Conj $ map (SVar . showCId) fs)
-
+-- | Function to remove underscores because they annoy LPSolve
 clean :: String -> String
 clean = filter (not . flip elem "_")
 
-addField :: String -> String -> String
-addField s1 s2
-  | length s2 > 8 = error "value too long"
-  | length s1 == 0 = " " ++ s2
-  | length s1 < 5  = s1 ++ align 5 (length s1) ++ s2
-  | length s1 < 15 = s1 ++ align 15 (length s1) ++ s2
-  | length s1 < 25 = s1 ++ align 25 (length s1) ++ s2
-  | length s1 < 40 = s1 ++ align 40 (length s1) ++ s2
-  | length s1 < 50 = s1 ++ align 50 (length s1) ++ s2
-  | otherwise      = s1 ++ "  " ++ s2
+
+-- | Function tp onverts a sat problem into MiniSat. The problem is stated in two parts, the
+-- | first is the list of premises, the second is the problem to be solved under the
+-- | premisses. The premises are supposed to be a conjunction
+satToMiniSat :: Solver -> (SAT String,SAT String) -> IO (M.Map String Lit, [Lit])
+satToMiniSat solv (prem@(Conj ps),cs) = do
+  -- convert all variables into literals and store references in a map
+  lits <- mkLits solv [prem,cs]
+  -- put the problem into the solver
+  satToMiniSat' solv lits cs
+  -- convert the premises
+  nprems <- mapM (satToMiniSat' solv lits) ps
+  return (lits,nprems)
   where
-    align x y = replicate (x - y - 1) ' '
+    -- Add a SAT term recursively to the solver and return the top-level literal
+    satToMiniSat' :: Solver -> M.Map String Lit -> SAT String -> IO Lit
+    satToMiniSat' solv lits cs =
+      case cs of
+        Conj ss ->
+          do
+            ls <- mapM (satToMiniSat' solv lits) ss              
+            c <- andl solv ls
+            addClause solv [c]
+            return c
+        Dis ss ->
+          do
+            ls <- mapM (satToMiniSat' solv lits) ss
+            c <- orl solv ls
+            addClause solv [c]
+            return c
+        Imp a b ->
+          do
+            a' <- satToMiniSat' solv lits a
+            b' <- satToMiniSat' solv lits b
+            c <- implies solv a' b'
+            addClause solv [c]
+            return c
+        SVar v ->
+          return $ fromJust $ M.lookup v lits -- this can be evil
+    -- Convert all variables in a list of SAT terms into literals and remembers the names in a map
+    mkLits :: Solver -> [SAT String] -> IO (M.Map String Lit)
+    mkLits s sats = M.unions <$> mapM (mkLits' s M.empty) sats
+    mkLits' :: Solver -> M.Map String Lit -> SAT String ->  IO (M.Map String Lit)
+    mkLits' s lits (Conj ss) = M.unions <$> mapM (mkLits' s lits) ss
+    mkLits' s lits (Dis ss) =  M.unions <$> mapM (mkLits' s lits) ss
+    mkLits' s lits (Imp a b) = liftM2 M.union (mkLits' s lits a) (mkLits' s lits b)
+    mkLits' s lits (SVar v) = do
+      l <- newLit s
+      return $ M.insert v l lits
+
+-- | Function to print a model given a solver and a map of literals
+printModel :: Solver -> M.Map String Lit -> IO ()
+printModel s ls =
+   putStrLn =<< (return . unlines) =<< sequence [fmap (((v ++ "\t") ++) . show) $ modelValue s l |  (v,l) <- M.toList ls]
